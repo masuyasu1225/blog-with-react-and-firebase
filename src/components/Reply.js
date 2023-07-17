265defc224266977fc0327987bca61882bcc4374
masu <yasumoto.7991mm@gmail.com>
2023-07-17 09:55:28 +0700

    add reply function

---
+++ src/components/Reply.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
+
+const CreateReply = ({ postId }) => {
+  const [replyText, setReplyText] = useState();
+
+  const createReply = async () => {
+    if (auth.currentUser) {
+      await addDoc(collection(db, "replies"), {
+        postId: postId,
+        replyText: replyText,
+        author: {
+          username: auth.currentUser.displayName,
+          id: auth.currentUser.uid,
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="createReplySection">
+      <div className="inputReply">
+        <div>リプライ</div>
+        <textarea
+          placeholder="リプライ内容を記入"
+          onChange={(e) => setReplyText(e.target.value)}
+        ></textarea>
+      </div>
+      <button className="replyButton" onClick={createReply}>
+        リプライする
+      </button>
+    </div>
+  );
+};
+
+export default CreateReply;
