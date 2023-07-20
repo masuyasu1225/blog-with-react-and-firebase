57f4efadcbd2c386e97dc3efcb16efc4c249a8b5
masu <yasumoto.7991mm@gmail.com>
2023-07-20 15:32:39 +0700

    create popup css

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import "./ConfirmModal.css";
 
 const ConfirmModal = ({ isOpen, onRequestClose, onConfirm }) => {
   return (
@@ -7,10 +8,18 @@ const ConfirmModal = ({ isOpen, onRequestClose, onConfirm }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Confirmation Modal"
+      className="confirmationModal"
+      overlayClassName="modalOverlay"
     >
-      <h2>この記事を削除していいですか？</h2>
-      <button onClick={onConfirm}>はい</button>
-      <button onClick={onRequestClose}>いいえ</button>
+      <h2>この記事を削除しますか？</h2>
+      <div className="confirmationButton">
+        <button onClick={onConfirm} className="yesButton">
+          削除
+        </button>
+        <button onClick={onRequestClose} className="noButton">
+          キャンセル
+        </button>
+      </div>
     </Modal>
   );
 };
