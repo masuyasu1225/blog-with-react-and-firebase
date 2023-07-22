0e9ce8282f1b407bb9f487b8e42101ab10304ae9
masu <yasumoto.7991mm@gmail.com>
2023-07-22 11:17:14 +0700

    use Bold, Italic, Underline

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
+import "draft-js/dist/Draft.css";
 import "./CreatePost.css";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { auth, db } from "../firebase";
@@ -6,14 +8,13 @@ import { useNavigate } from "react-router-dom";
 
 const CreatePost = ({ isAuth }) => {
   const [title, setTitle] = useState();
-  const [postText, setPostText] = useState();
-
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const navigate = useNavigate();
 
   const createPost = async () => {
     await addDoc(collection(db, "posts"), {
       title: title,
-      postsText: postText,
+      postsText: editorState.getCurrentContent().getPlainText("\u0001"),
       author: {
         username: auth.currentUser.displayName,
         id: auth.currentUser.uid,
@@ -22,6 +23,30 @@ const CreatePost = ({ isAuth }) => {
     });
 
     navigate("/");
+  };
+
+  // スタイルを適用する関数
+  const handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      setEditorState(newState);
+      return "handled";
+    }
+
+    return "not-handled";
+  };
+
+  const onBoldClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
+  const onItalicClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
+  };
+
+  const onUnderlineClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
   };
 
   useEffect(() => {
@@ -45,10 +70,17 @@ const CreatePost = ({ isAuth }) => {
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea
-            placeholder="投稿内容を記入"
-            onChange={(e) => setPostText(e.target.value)}
-          ></textarea>
+          <button onClick={onBoldClick}>Bold</button>
+          <button onClick={onItalicClick}>Italic</button>
+          <button onClick={onUnderlineClick}>Underline</button>
+          <div className="editor">
+            <Editor
+              editorState={editorState}
+              handleKeyCommand={handleKeyCommand}
+              onChange={setEditorState}
+              placeholder="投稿内容を記入"
+            />
+          </div>
         </div>
         <button className="postButton" onClick={createPost}>
           投稿する
